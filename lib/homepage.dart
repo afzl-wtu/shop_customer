42b0f654f102aa32dda590fdc0978a4c63bbda19
Muhammad Afzal Tahir <64379149+afzl-wtu@users.noreply.github.com>
2020-09-04 13:02:48 +0500

    working Condition

---
+++ lib/homepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import './screens/main_screen.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainScreen();
+  }
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Icon(
+          Icons.wallpaper,
+        ),
+      ),
+    );
+  }
+}
